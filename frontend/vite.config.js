44794c43a0b50be193890b9517a16d7c432fcfda
Gurruprasaath M K <155515899+gurruprasaath@users.noreply.github.com>
2026-03-26 20:54:26 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://consultancy-k8nb.onrender.com   use this as backend api',
         changeOrigin: true,
       },
     },
